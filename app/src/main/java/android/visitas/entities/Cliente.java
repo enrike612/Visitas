7304360fb3ddc5a9ec137e5f4b47798ddac13117
eramirezn <edrnrrr@gmail.com>
2015-02-25 00:23:23 -0500

    esto es una pruebaaaaa

---
+++ app/src/main/java/android/visitas/entities/Cliente.java
@@ -0,0 +1,20 @@
+package android.visitas.entities;
+
+/**
+ * Created by EduardoR on 25/02/2015.
+ */
+public class Cliente {
+    private String pruebita;
+
+    public Cliente(String pruebita) {
+        this.pruebita = pruebita;
+    }
+
+    public String getPruebita() {
+        return pruebita;
+    }
+
+    public void setPruebita(String pruebita) {
+        this.pruebita = pruebita;
+    }
+}
