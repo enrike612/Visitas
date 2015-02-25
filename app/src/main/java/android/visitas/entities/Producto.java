7f3413f3f8a735c89c22ded24a94e154307f7d0b
enrike612 <e@lapiz2b.com>
2015-02-25 00:06:08 -0500

    Agregué la entidad Producto para probar

---
+++ app/src/main/java/android/visitas/entities/Producto.java
@@ -0,0 +1,7 @@
+package android.visitas.entities;
+
+/**
+ * Created by ENRIQUE on 22/02/2015.
+ */
+public class Producto {
+}
---
+++ app/src/main/java/android/visitas/entities/Visita.java
@@ -0,0 +1,7 @@
+package android.visitas.entities;
+
+/**
+ * Created by ENRIQUE on 24/02/2015.
+ */
+public class Visita {
+}
